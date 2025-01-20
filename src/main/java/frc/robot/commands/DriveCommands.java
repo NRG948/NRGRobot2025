06c99c286d32aa56f8f4329ccf309f940aa1a2a2
Harry <oyjunhan@hotmail.com>
2025-01-19 18:17:45 -0800

    Implement PathPlanner-based reef alignment

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -7,10 +7,18 @@
  
 package frc.robot.commands;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathConstraints;
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants;
+import frc.robot.commands.AlignToReef.ReefBranch;
+import frc.robot.parameters.SwerveDriveParameters;
 import frc.robot.subsystems.Subsystems;
+import frc.robot.subsystems.SwerveSubsystem;
 
 public final class DriveCommands {
   /**
@@ -37,5 +45,28 @@ public final class DriveCommands {
 
   public static Command interruptAll(Subsystems subsystems) {
     return Commands.runOnce(() -> {}, subsystems.getAll());
+  /**
+   * Returns a command to follow the path to the specified branch of the nearest reef side.
+   *
+   * @param subsystems The Subsystems container.
+   * @param targetReefBranch The target reef branch (left or right).
+   * @return A command to follow the path to the specified branch of the nearest reef side.
+   */
+  public static Command alignToReefPP(Subsystems subsystems, ReefBranch targetReefBranch) {
+    SwerveSubsystem drivetrain = subsystems.drivetrain;
+    Pose2d currentRobotPose = drivetrain.getPosition();
+    int nearestTagId = AlignToReef2.findNearestReefTagID(currentRobotPose);
+    Pose2d targetPose =
+        Constants.VisionConstants.REEF_SCORING_POSES.get(
+            new Pair<Integer, ReefBranch>(nearestTagId, targetReefBranch));
+    SwerveDriveParameters currentSwerveParameters = SwerveSubsystem.PARAMETERS.getValue();
+
+    return AutoBuilder.pathfindToPose(
+        targetPose,
+        new PathConstraints(
+            SwerveSubsystem.getMaxSpeed() * 0.3,
+            SwerveSubsystem.getMaxAcceleration(),
+            currentSwerveParameters.getMaxRotationalSpeed() * 0.3,
+            currentSwerveParameters.getMaxRotationalAcceleration()));
   }
 }
