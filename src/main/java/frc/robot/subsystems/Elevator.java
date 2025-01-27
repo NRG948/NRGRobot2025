20cdf1d8138d6d03da16e722b0726ee776270717
NRG948DriverStation <programming@nrg948.com>
2025-01-26 17:02:33 -0800

    Tune elevator (not complete) Co-Authored-By: Harry695 <oyjunhan@hotmail.com> Co-Authored-By: Space9999 <sboddams@gmail.com>

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -8,20 +8,30 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.nrg948.preferences.RobotPreferences;
+import com.nrg948.preferences.RobotPreferencesLayout;
+import com.nrg948.preferences.RobotPreferencesValue;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.util.datalog.BooleanLogEntry;
 import edu.wpi.first.util.datalog.DoubleLogEntry;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.RobotConstants;
 import frc.robot.parameters.ElevatorLevel;
@@ -29,23 +39,35 @@ import frc.robot.util.LimitSwitch;
 import frc.robot.util.MotorController;
 import frc.robot.util.RelativeEncoder;
 import frc.robot.util.TalonFXAdapter;
+import java.util.Set;
 
-public class Elevator extends SubsystemBase {
+@RobotPreferencesLayout(groupName = "Elevator", row = 1, column = 0, width = 1, height = 2)
+public class Elevator extends SubsystemBase implements ShuffleboardProducer {
+  @RobotPreferencesValue
+  public static RobotPreferences.BooleanValue ENABLE_TAB =
+      new RobotPreferences.BooleanValue("Elevator", "Enable Tab", false);
 
   // physical parameters of the elevator
   private static final double GEAR_RATIO = (60.0 / 12.0) * (24.0 / 15.0);
-  private static final double SPROCKET_DIAMETER = 0.0475; // 4.75 cm
+  private static final double SPROCKET_DIAMETER = 0.05; // 5 cm
   private static final double MASS = 2.0; // kilograms
-  private static final double METERS_PER_REVOLUTION = (SPROCKET_DIAMETER * Math.PI) / GEAR_RATIO;
+  private static final double CARRIAGE_OFFSET = 0.033; // in meters
+  private static final double METERS_PER_REVOLUTION =
+      (((SPROCKET_DIAMETER * Math.PI) / GEAR_RATIO) * 2);
+  // The multiplication of 2 is to account for the second stage moving relative to the first stage
+  // and the first stage moving relative to the bottom
+  private static final double MAX_HEIGHT = 1.42;
+  private static final double MIN_HEIGHT = CARRIAGE_OFFSET + 0.002;
 
   // trapezoid profile values
   private static final DCMotor MOTOR_PARAMS = DCMotor.getKrakenX60(1);
   private static final double MAX_SPEED =
-      (MOTOR_PARAMS.freeSpeedRadPerSec * SPROCKET_DIAMETER) / (GEAR_RATIO * 2 * Math.PI); // m/s
+      (MOTOR_PARAMS.freeSpeedRadPerSec * SPROCKET_DIAMETER) / (GEAR_RATIO * 2); // m/s
   private static final double MAX_ACCELERATION =
-      (2 * MOTOR_PARAMS.stallTorqueNewtonMeters * GEAR_RATIO) / (SPROCKET_DIAMETER * MASS); // m/s^2
+      ((2 * MOTOR_PARAMS.stallTorqueNewtonMeters * GEAR_RATIO) / (SPROCKET_DIAMETER * MASS))
+          * 2; // m/s^2 for two motors
   private static final TrapezoidProfile.Constraints CONSTRAINTS =
-      new TrapezoidProfile.Constraints(MAX_SPEED, MAX_ACCELERATION);
+      new TrapezoidProfile.Constraints(MAX_SPEED / 2, MAX_ACCELERATION / 2);
 
   // feedforward constants
   private static final double KS = 0.15;
@@ -54,7 +76,7 @@ public class Elevator extends SubsystemBase {
   private static final double KG = 9.81 * KA;
 
   // feedback constants
-  private static final double KP = 1.0;
+  private static final double KP = 5.0;
   private static final double KI = 0;
   private static final double KD = 0;
 
@@ -133,14 +155,28 @@ public class Elevator extends SubsystemBase {
     logIsSeekingGoal.append(false);
   }
 
+  /**
+   * Sets elevator goal level.
+   *
+   * @param level
+   */
   public void setGoalPosition(ElevatorLevel level) {
+    setGoalPosition(level.getHeight());
+  }
+
+  /**
+   * Sets elevator goal height in meters.
+   *
+   * @param height
+   */
+  private void setGoalPosition(double height) {
     timer.reset();
     timer.start();
     isSeekingGoal = true;
-    goalState.position = level.getHeight();
+    goalState.position = height;
     goalState.velocity = 0;
     logIsSeekingGoal.append(true);
-    logGoalPosition.append(level.getHeight());
+    logGoalPosition.append(height);
     logGoalVelocity.append(0);
   }
 
@@ -158,8 +194,11 @@ public class Elevator extends SubsystemBase {
       currentState.position = simElevator.getPositionMeters();
       currentState.velocity = simElevator.getVelocityMetersPerSecond();
     }
-    atUpperLimit = upperLimit.isPressed();
-    atLowerLimit = lowerLimit.isPressed();
+    currentState.position += CARRIAGE_OFFSET;
+
+    atUpperLimit = currentState.position >= MAX_HEIGHT;
+    atLowerLimit = currentState.position <= MIN_HEIGHT;
+
     elevatorMech2d.setLength(currentState.position);
     logCurrentPosition.append(currentState.position);
     logCurrentVelocity.append(currentState.velocity);
@@ -194,5 +233,38 @@ public class Elevator extends SubsystemBase {
   public void simulationPeriodic() {
     simElevator.setInputVoltage(currentVoltage);
     simElevator.update(0.020);
+  }
+
+  @Override
+  public void addShuffleboardTab() {
+    if (!ENABLE_TAB.getValue()) {
+      return;
+    }
+
+    ShuffleboardTab elevatorTab = Shuffleboard.getTab("Elevator");
+    ShuffleboardLayout elevatorLayout =
+        elevatorTab.getLayout("Status", BuiltInLayouts.kList).withPosition(0, 0).withSize(2, 5);
+    elevatorLayout.addDouble("Current Height", () -> this.currentState.position);
+    elevatorLayout.addDouble("Current Velocity", () -> this.currentState.velocity);
+    elevatorLayout.addDouble("Goal Height", () -> this.goalState.position);
+    elevatorLayout.addDouble("Goal Velocity", () -> this.goalState.velocity);
+    elevatorLayout
+        .addBoolean("Is Enabled", () -> this.isSeekingGoal)
+        .withWidget(BuiltInWidgets.kBooleanBox);
+    elevatorLayout.add("Max Velocity", MAX_SPEED);
+    elevatorLayout.add("Max Acceleration", MAX_ACCELERATION);
+
+    ShuffleboardLayout controlLayout =
+        elevatorTab.getLayout("Control", BuiltInLayouts.kList).withPosition(2, 0).withSize(2, 5);
+    controlLayout.addDouble("Current Height", () -> this.currentState.position);
+    GenericEntry elevatorHeight = controlLayout.add("Elevator Height", 0).getEntry();
+    controlLayout.add(
+        Commands.defer(
+                () ->
+                    Commands.runOnce(() -> this.setGoalPosition(elevatorHeight.getDouble(0)))
+                        .until(() -> this.atGoalPosition()),
+                Set.of(this))
+            .withName("Set Height"));
+    controlLayout.add(Commands.runOnce(() -> this.disable(), this).withName("Disable"));
   }
 }
