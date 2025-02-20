2a0d19f13355026d0399e79bf3b64e8a3001d473
NRG948DriverStation <programming@nrg948.com>
2025-02-19 20:21:07 -0800

    Corrects maximum elevator height

--- src/main/java/frc/robot/parameters/ElevatorLevel.java
+++ src/main/java/frc/robot/parameters/ElevatorLevel.java
@@ -15,7 +15,7 @@ public enum ElevatorLevel {
   L1(0.375, Math.toRadians(60), .2),
   L2(0.37, Math.toRadians(60), .2),
   L3(0.78, Math.toRadians(60), .2),
-  L4(1.4, Math.toRadians(50), .2),
+  L4(1.33, Math.toRadians(50), .2),
 
   AlgaeL2(1.5, 0, .2), // TO-DO: Determine correct values for height and arm angles (algae removal)
   AlgaeL3(2.5, 0, .2);
