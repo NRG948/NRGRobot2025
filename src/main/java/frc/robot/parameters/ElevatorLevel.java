5cba2cf831eaaa8fdd6556f2553173c683f7902d
NRG948DriverStation <programming@nrg948.com>
2025-03-16 08:31:33 -0700

    Implements coral outtake speed for coral ElevatorLevel and fixes auto-centering

--- src/main/java/frc/robot/parameters/ElevatorLevel.java
+++ src/main/java/frc/robot/parameters/ElevatorLevel.java
@@ -10,19 +10,20 @@ package frc.robot.parameters;
 import frc.robot.subsystems.Elevator;
 
 public enum ElevatorLevel {
-  STOWED(Elevator.STOWED_HEIGHT_FOR_PID, Math.toRadians(92), 0, 0),
-  L1(0.15, Math.toRadians(36), .2, 0),
-  L2(0.33, Math.toRadians(60), .2, 0),
-  L3(0.74, Math.toRadians(60), .2, 0),
-  L4(1.34, Math.toRadians(50), .2, .5),
+  STOWED(Elevator.STOWED_HEIGHT_FOR_PID, Math.toRadians(92), 0, 0, 0),
+  L1(0.15, Math.toRadians(36), .2, 1.5, 0.3),
+  L2(0.33, Math.toRadians(60), .2, 0, 1.5),
+  L3(0.74, Math.toRadians(60), .2, 0, 1.5),
+  L4(1.34, Math.toRadians(50), .2, .1, 2.0),
 
-  AlgaeL2(0.25, Math.toRadians(40), .2, 0),
-  AlgaeL3(0.65, Math.toRadians(40), .2, 0);
+  AlgaeL2(0.25, Math.toRadians(40), .2, 0, 2.0),
+  AlgaeL3(0.65, Math.toRadians(40), .2, 0, 2.0);
 
   private final double elevatorHeight;
   private final double armAngle;
   private final double armOffset;
-  private double outtakeDelay;
+  private final double outtakeDelay;
+  private final double outtakeSpeed;
 
   /**
    * Constructs a variant of this enum.
@@ -30,11 +31,13 @@ public enum ElevatorLevel {
    * @param height The desired height in meters.
    * @param armAngle The desired arm angle in radians.
    */
-  private ElevatorLevel(double height, double armAngle, double armOffset, double outtakeDelay) {
+  private ElevatorLevel(
+      double height, double armAngle, double armOffset, double outtakeDelay, double outtakeSpeed) {
     this.elevatorHeight = height;
     this.armAngle = armAngle;
     this.armOffset = armOffset;
     this.outtakeDelay = outtakeDelay;
+    this.outtakeSpeed = outtakeSpeed;
   }
 
   /** Returns the desired height in meters. */
@@ -54,18 +57,7 @@ public enum ElevatorLevel {
 
   /** Returns the outtake speed in meters per second. */
   public double getOuttakeSpeed() {
-    switch (this) {
-      case L1:
-        return 0.2;
-      case L4:
-        return 2.0;
-      case L2:
-        return 1.0;
-      case L3:
-        return 1.0;
-      default:
-        return 2.0;
-    }
+    return outtakeSpeed;
   }
 
   /** Gets the outtake delay in seconds. */
