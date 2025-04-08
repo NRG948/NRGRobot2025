0406a032125518622cd64ef1a26e4d7a36134705
NRG948DriverStation <programming@nrg948.com>
2025-04-08 10:54:47 -0700

    Rounds camera transforms to 3 sig figs

--- src/main/java/frc/robot/subsystems/AprilTag.java
+++ src/main/java/frc/robot/subsystems/AprilTag.java
@@ -77,18 +77,18 @@ public class AprilTag extends SubsystemBase implements ShuffleboardProducer {
       new Rotation3d(0, toRadians(20), toRadians(15));
   public static final Transform3d PRACTICE_ROBOT_TO_FRONT_RIGHT_CAMERA =
       new Transform3d(
-          new Translation3d(+0.273558, +0.286004, +0.19685), FRONT_RIGHT_CAMERA_ROTATION);
+          new Translation3d(+0.274, +0.286, +0.197), FRONT_RIGHT_CAMERA_ROTATION);
   public static final Transform3d COMPETITION_ROBOT_TO_FRONT_RIGHT_CAMERA =
       new Transform3d(
-          new Translation3d(+0.273558, -0.286004, +0.19685), FRONT_RIGHT_CAMERA_ROTATION);
+          new Translation3d(+0.274, -0.286, +0.197), FRONT_RIGHT_CAMERA_ROTATION);
 
   private static final Rotation3d FRONT_LEFT_CAMERA_ROTATION = new Rotation3d(0, toRadians(20), toRadians(-15));
   public static final Transform3d PRACTICE_ROBOT_TO_FRONT_LEFT_CAMERA =
       new Transform3d(
-          new Translation3d(+0.273558, +0.286004, +0.19685), FRONT_LEFT_CAMERA_ROTATION);
+          new Translation3d(+0.274, +0.286, +0.197), FRONT_LEFT_CAMERA_ROTATION);
   public static final Transform3d COMPETITION_ROBOT_TO_FRONT_LEFT_CAMERA =
       new Transform3d(
-          new Translation3d(+0.273558, +0.286004, +0.19685), FRONT_LEFT_CAMERA_ROTATION);
+          new Translation3d(+0.274, +0.286, +0.197), FRONT_LEFT_CAMERA_ROTATION);
 
   /**
    * The robot's vision parameters.
